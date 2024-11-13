d0e315810263c60ba96c35148e9fbc87886b4106
Jakhon Nematov <nj@exante.eu>
2024-11-13 18:37:14 +0500

    player stop after dissmiss

--- Sources/PlayerKit/UI Controls/Views/CloseButtonView.swift
+++ Sources/PlayerKit/UI Controls/Views/CloseButtonView.swift
@@ -1,12 +1,12 @@
 import SwiftUI
 
 public struct CloseButtonView: View {
-    @Environment(\.presentationMode) private var presentationMode // Environment variable for dismissing
-
-    public init() {}
+    @Environment(\.presentationMode) private var presentationMode
+    @ObservedObject var playerManager: PlayerManager = PlayerManager.shared // Pass PlayerManager here
 
     public var body: some View {
         Button(action: {
+            playerManager.stop() // Stop the player
             presentationMode.wrappedValue.dismiss() // Dismiss the view
         }) {
             Image(systemName: "xmark.circle.fill")
