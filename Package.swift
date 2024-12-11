af2214b0b98fa71e163483146aebbbcbf1c5add4
Jakhon Nematov <nj@exante.eu>
2024-12-11 20:31:54 +0500

    Bump Tag to v1.0.3

--- Package.swift
+++ Package.swift
@@ -11,12 +11,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "VLCKit",
-            url: "https://github.com/jakhongir97/PlayerKit/releases/download/1.0.2/VLCKit.xcframework.zip",
+            url: "https://github.com/jakhongir97/PlayerKit/releases/download/1.0.3/VLCKit.xcframework.zip",
             checksum: "8f9989c70697ab64c378862c1d8f5fa696bb853db7c6294eb113b69e6140fed3"
         ),
         .binaryTarget(
             name: "GoogleCast",
-            url: "https://github.com/jakhongir97/PlayerKit/releases/download/1.0.2/GoogleCast.xcframework.zip",
+            url: "https://github.com/jakhongir97/PlayerKit/releases/download/1.0.3/GoogleCast.xcframework.zip",
             checksum: "64a2e1bf3e92bbe82e105cda874a00731406593b13d5112ccf39a8ea53477edd"
         ),
         .target(
