bc60ed0bc6e44bce6555c9ffd7570b11517865df
Jakhon Nematov <nj@exante.eu>
2025-02-11 18:47:26 +0500

    Updated VLCKit library

--- Package.swift
+++ Package.swift
@@ -11,13 +11,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "VLCKit",
-            url: "https://github.com/jakhongir97/PlayerKit/releases/download/1.0.3/VLCKit.xcframework.zip",
-            checksum: "8f9989c70697ab64c378862c1d8f5fa696bb853db7c6294eb113b69e6140fed3"
+            url: "https://github.com/jakhongir97/PlayerKit/releases/download/1.0.7/VLCKit.xcframework.zip",
+            checksum: "2bb6de2ccd80a972cec24f19a2e1ecd3829eb87c6ea972cb39ca8c7c3968d997"
         ),
         .binaryTarget(
             name: "GoogleCast",
-            url: "https://github.com/jakhongir97/PlayerKit/releases/download/1.0.3/GoogleCast.xcframework.zip",
-            checksum: "64a2e1bf3e92bbe82e105cda874a00731406593b13d5112ccf39a8ea53477edd"
+            url: "https://github.com/jakhongir97/PlayerKit/releases/download/1.0.7/GoogleCast.xcframework.zip",
+            checksum: "21090c27acb00c9576e44c4af084c473509ba6b9dd494d23b53f5390b0bcad91"
         ),
         .target(
             name: "PlayerKit",
